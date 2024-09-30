24ee1657eb994962e40c2e75153551ed6e7bf7d1
AlessioMura <alessiomura03@gmail.com>
2024-09-30 15:57:08 +0200

    calc ticket_price

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let price_for_km = 0.21;
 let disc_underage = 20;
 let disc_over65 = 40;
 let percent = 100;
+
+distance_km = prompt("Inserire i km che il treno percorre");
+const ticket_price = price_for_km * distance_km;
+alert(ticket_price + "€");
